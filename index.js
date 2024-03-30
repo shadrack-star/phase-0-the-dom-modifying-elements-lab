02c03a26cfedcf9d3a9e157f05d5b331bfac8c82
shadrack-star <shaddkiplangat98@gmail.com>
2024-03-30 12:48:03 +0300

    creates new elements

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+  document.querySelector("main#main").remove();
+  /* Creating new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
